763dcf36d8a5fefb99546f31aef0302c0fe95773
rbejar <rbejar@unizar.es>
2013-11-28 17:52:53 +0100

    Skeleton for the JavaScript module

---
+++ WebGLTests/src/richpanorama.js
@@ -0,0 +1,25 @@
+/**
+    RichPanorama.js - Rich Panorama Visualization
+    Copyright (C) 2013 Rubén Béjar {http://www.rubenbejar.com/}
+ */
+
+function RichPanoramaModule() {
+  var RichPanorama = {};
+  
+  function privateFunction() {
+  };
+  
+  var privateAttribute = 0;
+  
+  
+  RichPanorama.attribute = 0;
+  
+  RichPanorama.aFunction = function() {          
+  };
+
+
+
+  return RichPanorama;
+};
+
+var RichPanorama = RichPanoramaModule();
